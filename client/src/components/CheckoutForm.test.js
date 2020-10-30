918d88af9f348913ba77912bc65ac6478b161ed6
Wesley-Ryan <im.wesleyryan@gmail.com>
2020-10-30 09:40:22 -0700

    Added test id for Code Grade

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
@@ -14,13 +14,22 @@ test("form header renders", () => {
 
 
 test("form shows success message on submit with form details", () => {
-
+    render(<CheckoutForm />)
     const firstNameInput = screen.queryByLabelText('First Name');
     const lastNameInput = screen.queryByLabelText('Last Name');
     const addressInput = screen.queryByLabelText('Address');
     const cityInput = screen.queryByLabelText('City');
     const stateInput = screen.queryByLabelText('State');
-    const zipInput = screen.queryByLabelText('Zip');
+    
+
+    fireEvent.change(firstNameInput,{ target:{ value: 'canine', name:'firstName'}})
+
+
+    const button = screen.getByRole("button");
+    fireEvent.click(button);
+
+
+
 
 
 
